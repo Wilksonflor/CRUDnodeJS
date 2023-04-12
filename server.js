ecdbf77ad2a072d1351e5843fbc8f9d02cf14378
Wilkson Flor <wilksonflor12@gmail.com>
2023-04-11 21:56:12 -0300

    Erro no mongoose - serverjs

--- server.js
+++ server.js
@@ -1,41 +1,48 @@
-const express = require('express');
+const express = require("express");
 // Express => Servidor para pegar as rotas e definir a aplicação
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 // CookieParser => Para a troca de dados
-const cors = require('cors')
+const cors = require("cors");
 // Cors => É para compartilhar dados entre os dominios para não ter conflitos
-const path = require('path')
+const path = require("path");
 // path => Vem direto do Node. É usado para usar as pastas
-const mongoose = require('mongoose')
 
-const routes = require('./src/routes')
+const mongoose = require("mongoose");
 
-const app = express()
-// Para 'chamar o express' colocando ele em uma variável; 
+const routes = require("./src/routes");
 
+const app = express();
+// Para 'chamar o express' colocando ele em uma variável;
 
-// mongoose.connect('wlkdb.web3gmq.mongodb.net:27017', {
-//     useUnifieldTopoloy: true,
-//     useNewUrlParser: true,
-//     useFindAndModify: false
-// }
+mongoose.connect(
+  "mongodb://localhost:27017/baseCrud",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  },
+  function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("MongoDB conectado com sucesso");
+    }
+  }
+);
+
 // Iniciando a conexão com o banco de dados
 
-
-
-app.use(cors())
+app.use(cors());
 // Sempre que for biblioteca chama como fosse uma função.
-app.use(cookieParser())
-app.use(express.json())
+app.use(cookieParser());
+app.use(express.json());
 // Para pegar os dados e enviar os dados vai ser um JSON
 app.use(routes);
 // Não é uma biblioteca, então não precisa ser como função
 
-
-
-app.listen(3003, function(){
-    console.log('Servidor Iniciado...')
-})
+app.listen(3003, function () {
+  console.log("Servidor iniciado com sucesso");
+});
 
 // Primeiro parametro é a porta
 // Segundo parametro é a função que vai executar na porta
